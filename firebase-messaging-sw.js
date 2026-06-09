40a8b1ee72b4104d5662b6d54ac86948a5e52f62
lrdmbl009-ai <lrdmbl009@gmail.com>
2026-06-09 23:22:00 +0800

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,30 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyD93ILJjfqLAoLosdXn4Gm_0F20WFiYz2I",
   authDomain: "akamsiperdos-84b0b.firebaseapp.com",
   projectId: "akamsiperdos-84b0b",
   storageBucket: "akamsiperdos-84b0b.firebasestorage.app",
   messagingSenderId: "354170675681",
   appId: "1:354170675681:web:33204d35fa6d3e25f732a0"
-});
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(
-    payload.notification?.title || "AKAMSI",
-    {
-      body: payload.notification?.body || "Ada order baru",
-      icon: "/driver-192.png",
-      badge: "/driver-192.png"
-    }
-  );
+  console.log('[FCM Background] Pesan diterima:', payload);
+  
+  const notificationTitle = payload.notification?.title || "AKAMSI";
+  const notificationOptions = {
+    body: payload.notification?.body || "Ada update baru",
+    icon: "./customer-192.png",
+    badge: "./customer-192.png",
+    vibrate: [200, 100, 200],
+    data: payload.data || {},
+    requireInteraction: true
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
